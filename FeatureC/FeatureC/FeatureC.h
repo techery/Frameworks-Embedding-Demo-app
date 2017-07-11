1e6f6b601e71dc5b441fd02fa0869e2c1e761cdf
Eugene Tulusha <tulusheg@gmail.com>
2017-07-11 13:28:25 +0300

    Add feature C as an example

---
+++ FeatureC/FeatureC/FeatureC.h
@@ -0,0 +1,19 @@
+//
+//  FeatureC.h
+//  FeatureC
+//
+//  Created by Eugene on 7/11/17.
+//  Copyright © 2017 Techery, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FeatureC.
+FOUNDATION_EXPORT double FeatureCVersionNumber;
+
+//! Project version string for FeatureC.
+FOUNDATION_EXPORT const unsigned char FeatureCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeatureC/PublicHeader.h>
+
+
